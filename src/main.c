d05a64d3e1f9d2b3914d47fd77f86c5ddbe21ee6
Aaron <aaron.manuel@ucalgary.ca>
2022-12-12 13:02:32 -0700

    Finished section 5

--- src/main.c
+++ src/main.c
@@ -2,6 +2,8 @@
 #include <SDL2/SDL.h>
 #include "./constants.h"
 
+//That's a lot of globals.
+int game_is_running = FALSE;
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
 
@@ -10,6 +12,7 @@ int initialize_window(void){
         fprintf(stderr, "Error initializing SDL.\n");
         return FALSE;
     }
+    //Centre the window
     window = SDL_CreateWindow(
         NULL, 
         SDL_WINDOWPOS_CENTERED, 
@@ -22,16 +25,69 @@ int initialize_window(void){
             fprintf(stderr, "Error creating SDL Window.\n");
             return FALSE;
         }
+        //Render the window by attaching the window to renderer with default stuff.
         renderer = SDL_CreateRenderer(window, -1, 0);
-        if(!renderer{
-            fprintf(stderr, "Error creating SDL Renderer.\n")
+        if(!renderer){
+            fprintf(stderr, "Error creating SDL Renderer.\n");
             return FALSE;
-        })
+        }
 
     return TRUE;
 }
 
+void process_input(){
+    //Poll the event with reference to raw struct
+    SDL_Event event;
+    SDL_PollEvent(&event);
+    
+    //What type of event is it?
+    switch(event.type){
+        case SDL_QUIT:  //Click x button.
+            game_is_running = FALSE;
+            break;
+        case SDL_KEYDOWN:   //press esc key.
+            if(event.key.keysym.sym == SDLK_ESCAPE)
+            game_is_running = FALSE;
+            break;
+        
+    }
+
+}
+
+void setup(){
+
+}
+
+void update(){
+
+}
+
+void render(){
+
+}
+
+void destroy_window(){
+    //Destroy in reverse order of the init.
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
 int main(){
-    initialize_window();
+    //Setup
+    int game_is_running = initialize_window();
+
+    setup();
+
+    //loop forever
+    while(game_is_running){
+        //The holy trinity of the game loop.
+        process_input();
+        update();
+        render();
+    }
     return 0;
+
+    //This window will self destruct.
+    destroy_window();
 }
